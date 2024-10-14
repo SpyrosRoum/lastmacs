7912cf6a988839825916c77a7441abe435605673
Spyros Roum <spyros.roum@posteo.net>
2024-10-14 16:38:08 +0300

    Use basedpyright when available

--- init.el
+++ init.el
@@ -537,3 +537,14 @@
 (use-package
   nerd-icons-dired
   :hook (dired-mode . nerd-icons-dired-mode))
+
+(use-package
+  eglot
+  :straight nil
+  :ensure nil
+  :config
+  ;; Used basedpyright when it's available instead of pyright
+  (when (executable-find "basedpyright-langserver")
+    (add-to-list
+      'eglot-server-programs
+      '(python-base-mode . ("basedpyright-langserver" "--stdio")))))
