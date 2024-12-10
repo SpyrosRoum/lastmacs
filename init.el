682afe140732d6385b25b935014ebcb3c99c3b08
Spyros Roum <spyros.roum@posteo.net>
2024-12-10 10:55:49 +0200

    Switch to ef-theme

--- init.el
+++ init.el
@@ -11,20 +11,7 @@
 
 (use-package embark :bind (("C-c ." . embark-act)))
 
-(use-package
-  doom-themes
-  :config
-  ;; Global settings (defaults)
-  (setq
-    doom-themes-enable-bold t ; if nil, bold is universally disabled
-    doom-themes-enable-italic t) ; if nil, italics is universally disabled
-
-  (load-theme 'doom-dracula t)
-
-  ;; Enable flashing mode-line on errors
-  (doom-themes-visual-bell-config)
-  ;; Corrects (and improves) org-mode's native fontification.
-  (doom-themes-org-config))
+(use-package ef-themes :config (ef-themes-select 'ef-dream))
 
 (use-package
   ef-themes
@@ -400,4 +387,3 @@
 
 (use-package clojure-mode)
 (use-package cider)
-
