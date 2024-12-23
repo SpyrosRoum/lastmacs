859db9a7402c3f81adb258469e2d4ad08927dc17
Spyros Roum <spyros.roum@posteo.net>
2024-12-23 11:38:52 +0200

    Use advice-add to properly override compilation-read-command with completing version

--- lisp/init-emacs.el
+++ lisp/init-emacs.el
@@ -124,12 +124,16 @@
 
 ;; compilation-read-command uses `read-shell-command` by default, which doesn't use
 ;; completion at all. So I overwrite it to use `completing-read` instead, which seems to work great.
-(defun compilation-read-command (command)
+(defun compilation-read-command-with-autocomplete (command)
+  "Use `completing-read` to add autocomplete powers to compilation read"
   (completing-read "Compile command: " compile-history
     nil nil command
     (if (equal (car compile-history) command)
       '(compile-history . 1)
       'compile-history)))
 
+(advice-add
+  #'compilation-read-command
+  :override #'compilation-read-command-with-autocomplete)
 
 (provide 'init-emacs)
