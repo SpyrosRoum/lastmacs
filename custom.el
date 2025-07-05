677bbeab60e57b2170d797bfc2ac8a652812e65e
Spyros Roum <spyros.roum@posteo.net>
2025-07-05 11:26:12 +0300

    custom.el

--- custom.el
+++ custom.el
@@ -1,23 +1,28 @@
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
-  '
-  (custom-safe-themes
-    '
-    ("603a831e0f2e466480cdc633ba37a0b1ae3c3e9a4e90183833bc4def3421a961"
-      default))
-  '(xref-search-program 'ripgrep))
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("603a831e0f2e466480cdc633ba37a0b1ae3c3e9a4e90183833bc4def3421a961"
+     default))
+ '(safe-local-variable-values
+   '((eval and buffer-file-name
+	   (not (eq major-mode 'package-recipe-mode))
+	   (or (require 'package-recipe-mode nil t)
+	       (let ((load-path (cons "../package-build" load-path)))
+		 (require 'package-recipe-mode nil t)))
+	   (package-recipe-mode))))
+ '(xref-search-program 'ripgrep))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
-  '(evil-goggles-change-face ((t (:inherit diff-removed))))
-  '(evil-goggles-delete-face ((t (:inherit diff-removed))))
-  '(evil-goggles-paste-face ((t (:inherit diff-added))))
-  '(evil-goggles-undo-redo-add-face ((t (:inherit diff-added))))
-  '(evil-goggles-undo-redo-change-face ((t (:inherit diff-changed))))
-  '(evil-goggles-undo-redo-remove-face ((t (:inherit diff-removed))))
-  '(evil-goggles-yank-face ((t (:inherit diff-changed)))))
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(evil-goggles-change-face ((t (:inherit diff-removed))))
+ '(evil-goggles-delete-face ((t (:inherit diff-removed))))
+ '(evil-goggles-paste-face ((t (:inherit diff-added))))
+ '(evil-goggles-undo-redo-add-face ((t (:inherit diff-added))))
+ '(evil-goggles-undo-redo-change-face ((t (:inherit diff-changed))))
+ '(evil-goggles-undo-redo-remove-face ((t (:inherit diff-removed))))
+ '(evil-goggles-yank-face ((t (:inherit diff-changed)))))
