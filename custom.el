f7c8637ab099b7739eca801ceae87165b09ae4ea
Spyros Roum <spyros.roum@posteo.net>
2026-09-08 10:56:17 +0300

    custom.el

--- custom.el
+++ custom.el
@@ -1,77 +1,38 @@
 ;;; -*- lexical-binding: t -*-
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
-  '
-  (package-selected-packages
-    '
-    (avy
-      breadcrumb
-      cape
-      cider
-      clojure-ts-mode
-      company-box
-      corfu
-      dashboard
-      docker
-      docker-compose-mode
-      dockerfile-mode
-      doom-modeline
-      eca
-      ef-themes
-      elisp-autofmt
-      embark-consult
-      envrc
-      evil-collection
-      evil-goggles
-      exec-path-from-shell
-      gcmh
-      general
-      git-gutter
-      helpful
-      hl-todo
-      just-ts-mode
-      kind-icon
-      ligature
-      magit
-      marginalia
-      nerd-icons-completion
-      nerd-icons-dired
-      no-littering
-      olivetti
-      orderless
-      org-roam
-      otpp
-      ruff-format
-      rust-mode
-      scratchpads
-      smartparens
-      symbol-overlay
-      terraform-mode
-      ultra-scroll
-      undo-fu
-      vertico
-      vterm-toggle
-      vundo
-      wgrep))
-  '
-  (package-vc-selected-packages
-    '
-    (
-      (scratchpads
-        :url "https://github.com/SpyrosRoum/emacs-scratchpads"
-        :branch "master"))))
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(avy beancount breadcrumb cape cider clojure-ts-mode company-box
+	 corfu dashboard docker docker-compose-mode dockerfile-mode
+	 doom-modeline eca ef-themes eglot elisp-autofmt
+	 embark-consult envrc evil-collection evil-ghostel
+	 evil-goggles exec-path-from-shell gcmh general ghostel
+	 git-gutter helpful hl-todo just-ts-mode kind-icon
+	 kotlin-ts-mode kotlint-ts-mode ledger-mode ligature magit
+	 marginalia msgpack nerd-icons-completion nerd-icons-dired
+	 no-littering olivetti orderless org-roam otpp popper
+	 ruff-format rust-mode scratchpads smartparens svelte-ts-mode
+	 symbol-overlay terraform-mode tramp-hlo ultra-scroll undo-fu
+	 verb vertico vterm-toggle vundo web-mode wgrep))
+ '(package-vc-selected-packages
+   '((svelte-ts-mode :url "https://github.com/leafOfTree/svelte-ts-mode"
+		     :branch "main")
+     (scratchpads :url
+		  "https://github.com/SpyrosRoum/emacs-scratchpads"
+		  :branch "master")))
+ '(switch-to-buffer-in-dedicated-window 'pop))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
-  '(evil-goggles-change-face ((t (:inherit diff-removed))))
-  '(evil-goggles-delete-face ((t (:inherit diff-removed))))
-  '(evil-goggles-paste-face ((t (:inherit diff-added))))
-  '(evil-goggles-undo-redo-add-face ((t (:inherit diff-added))))
-  '(evil-goggles-undo-redo-change-face ((t (:inherit diff-changed))))
-  '(evil-goggles-undo-redo-remove-face ((t (:inherit diff-removed))))
-  '(evil-goggles-yank-face ((t (:inherit diff-changed)))))
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(evil-goggles-change-face ((t (:inherit diff-removed))))
+ '(evil-goggles-delete-face ((t (:inherit diff-removed))))
+ '(evil-goggles-paste-face ((t (:inherit diff-added))))
+ '(evil-goggles-undo-redo-add-face ((t (:inherit diff-added))))
+ '(evil-goggles-undo-redo-change-face ((t (:inherit diff-changed))))
+ '(evil-goggles-undo-redo-remove-face ((t (:inherit diff-removed))))
+ '(evil-goggles-yank-face ((t (:inherit diff-changed)))))
